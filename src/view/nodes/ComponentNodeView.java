9d2ec558082ade27103fe1f1528fef3ef20eb9b9
FlorianKas <florian.kasper@rwth-aachen.de>
2018-03-19 10:30:28 +0100

    Modified Views

--- src/view/nodes/ComponentNodeView.java
+++ src/view/nodes/ComponentNodeView.java
@@ -1,6 +1,21 @@
-package com.octouml.language.montiarc;
+package view.nodes;
 
-public class ComponentNodeView {
+import view.nodes.PortNodeView;
+import model.nodes.ComponentNode;
+import util.Constants;
+
+import java.beans.PropertyChangeEvent;
+
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle; 
+
+public class ComponentNodeView extends AbstractNodeView{
 // Visuell Representation of a ComponentNode
 	
 	// Shape C = Shape.union(A, B);
