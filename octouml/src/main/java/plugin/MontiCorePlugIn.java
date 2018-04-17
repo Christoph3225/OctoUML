8cb5f74346d63c96e7524b428ba31e06ab68d921
awortmann <wortmann@se-rwth.de>
2018-04-17 16:56:53 +0200

    working

---
+++ octouml/src/main/java/plugin/MontiCorePlugIn.java
@@ -0,0 +1,43 @@
+package plugin;
+
+import java.util.List;
+
+import controller.AbstractDiagramController;
+import de.monticore.ast.ASTNode;
+import de.monticore.prettyprint.IndentPrinter;
+import model.Graph;
+
+public interface MontiCorePlugIn {
+	// returns the diagram controller
+    AbstractDiagramController getController();
+    
+    // returns the name of the fxml file
+    String getView();
+    
+    // returns the name of the DSL
+    String getDSLName();
+    
+    // returns the file ending to the DSL for saving models
+    String getFileEnding();
+    
+    // returns the path to the picture
+    String getDSLPicture();
+    
+    // returns the prettyprinter of the DSL
+    IndentPrinter getPrettyPrinter();
+    
+    // returns the type of AST node
+    Class getASTNode();
+    
+    // returns the transformed (model to model) AST from the OctoUML Graph
+    ASTNode shapeToAST(Graph graph);
+    
+    // returns a list of errors
+    List<String> check(ASTNode node);
+    
+    // returns the generator of the DSL
+    String getGenerator();
+    
+    // returns true if generation of code was successful
+    boolean generateCode(ASTNode node, String path);
+}
