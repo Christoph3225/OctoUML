4036c97a4da350a4e99699e3b2fd2d483500d8b3
Christoph Quinten <christoph.quinten@rwth-aachen.de>
2018-05-22 15:30:19 +0200

    fixing the error

--- diagram-control/src/main/java/plugin/MontiCoreException.java
+++ diagram-control/src/main/java/plugin/MontiCoreException.java
@@ -3,6 +3,8 @@ package plugin;
 import javafx.scene.layout.Pane;
 import model.nodes.AbstractNode;
 
+//push comment
+
 public interface MontiCoreException {
   AbstractNode getNode();
   Pane getContentPane();
